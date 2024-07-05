382478027227eb2fc1e699c907386d95d7e21c40
Nimsew99 <112069750+Nimsew99@users.noreply.github.com>
2024-07-05 11:52:06 +0530

    Test Context Menu

--- src/test/java/contextMenu/ContextMenuTests.java
+++ src/test/java/contextMenu/ContextMenuTests.java
@@ -1,17 +1,18 @@
 package contextMenu;
 
-import keys.Test;
+import org.testng.annotations.Test;
 import base.BaseTests;
 import static org.testng.Assert.assertEquals;
 
 public class ContextMenuTests extends BaseTests{
 
     @Test
-    public void testRightClick(){
+    public void testRightClick() {
         var ContextMenuPage = homePage.clickContextMenu();
         ContextMenuPage.rightClickInBox();
         String message = ContextMenuPage.getPopupText();
         ContextMenuPage.acceptPopup();
         assertEquals(message, "You selected a context menu", "Popup message incorrect");
+
     }
 }
